fee17d567524d81bb072dbbdb562094456e56b1a
Florencia <nora.bookmessage@gmail.com>
2026-07-01 10:40:34 -0300

    test: add successful login smoke test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import LoginPage from "../pages/LoginPage";
+
+describe("SauceDemo Login Smoke Test", () => {
+  const loginPage = new LoginPage();
+
+  beforeEach(() => {
+    cy.fixture("users").as("users");
+    loginPage.visit();
+  });
+
+  it("Login successfull with valid credentials", function () {
+    loginPage.login(
+      this.users.validUser.username,
+      this.users.validUser.password
+    );
+
+    cy.url().should("include", "/inventory.html");
+    cy.get('[data-test="title"]').should("have.text", "Products");
+  });
+});
---
+++ cypress/pages/LoginPage.js
@@ -0,0 +1,26 @@
+class LoginPage {
+  visit() {
+    cy.visit('/');
+  }
+
+  enterUsername(username) {
+    cy.get('[data-test="username"]').clear().type(username);
+  }
+
+  enterPassword(password) {
+    cy.get('[data-test="password"]').clear().type(password);
+  }
+
+  clickLoginButton() {
+    cy.get('[data-test="login-button"]').click();
+  }
+
+  login(username, password) {
+    this.enterUsername(username);
+    this.enterPassword(password);
+    this.clickLoginButton();
+  }
+
+}
+
+export default LoginPage;
